344e3e51f5a741e0902e6b783b0145b33138b7b0
Donnixafr <donnixajj@gmail.com>
2020-04-24 11:28:19 +0700

    third commit

--- resources/views/survey/add.blade.php
+++ resources/views/survey/add.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Survey Covid-19</title>
+	<title>Stop Covid-19</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
